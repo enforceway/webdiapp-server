33bd31a14e72272aff53e2625ba11f591e873e45
enforceway <enforceway@163.com>
2019-03-25 23:15:29 +0800

    remove test goods code.

--- src/test/java/com/webdiapp/goods/TestGoods.java
+++ src/test/java/com/webdiapp/goods/TestGoods.java
@@ -1,94 +1,94 @@
-package com.webdiapp.goods;
-
-import java.util.List;
-import org.apache.ibatis.session.SqlSession;
-import org.junit.Test;
-
-import com.webdiapp.entities.Goods;
-import com.webdiapp.mapper.GoodsDAO;
-import com.webdiapp.util.MyBatisUtil;
-
-import junit.framework.Assert;
-
-public class TestGoods{
-
-    @Test
-    public void getGoodsPagerTest() {
-        int skip=4;
-        int size=2;
-        SqlSession session=MyBatisUtil.getSession();
-        try {
-            GoodsDAO bookdao=session.getMapper(GoodsDAO.class);
-            List<Goods> goods=bookdao.getGoodsPager(skip, size);
-            Assert.assertEquals(2, goods.size());
-        } finally {
-            session.close();
-        }
-    }
+//package com.webdiapp.goods;
+//
+//import java.util.List;
+//import org.apache.ibatis.session.SqlSession;
+//import org.junit.Test;
+//
+//import com.webdiapp.entities.Goods;
+//import com.webdiapp.mapper.GoodsDAO;
+//import com.webdiapp.util.MyBatisUtil;
+//
+//import junit.framework.Assert;
+//
+//public class TestGoods{
 //
 //    @Test
-//    public void getGoodsByIdTest() {
+//    public void getGoodsPagerTest() {
+//        int skip=4;
+//        int size=2;
 //        SqlSession session=MyBatisUtil.getSession();
 //        try {
 //            GoodsDAO bookdao=session.getMapper(GoodsDAO.class);
-//            Goods goods=bookdao.getGoodsById(1);
-//            Assert.assertEquals(1, goods.getId());
+//            List<Goods> goods=bookdao.getGoodsPager(skip, size);
+//            Assert.assertEquals(2, goods.size());
 //        } finally {
 //            session.close();
 //        }
 //    }
+////
+////    @Test
+////    public void getGoodsByIdTest() {
+////        SqlSession session=MyBatisUtil.getSession();
+////        try {
+////            GoodsDAO bookdao=session.getMapper(GoodsDAO.class);
+////            Goods goods=bookdao.getGoodsById(1);
+////            Assert.assertEquals(1, goods.getId());
+////        } finally {
+////            session.close();
+////        }
+////    }
+////    
+////    @Test
+////    public void getGoodsCountTest() {
+////        SqlSession session=MyBatisUtil.getSession();
+////        try {
+////            GoodsDAO bookdao=session.getMapper(GoodsDAO.class);
+////            Assert.assertEquals(9, bookdao.getGoodsCount());
+////        } finally {
+////            session.close();
+////        }
+////    }
+////    
+////    @Test
+////    public void insertTest() {
+////        SqlSession session=MyBatisUtil.getSession();
+////        try {
+////            Goods entity=new Goods();
+////            entity.setName("m俺妈说说名字必须的长");
+////            entity.setPrice(108);
+////            entity.setPicture("nopic.jpg");
+////            GoodsDAO bookdao=session.getMapper(GoodsDAO.class);
+////            Assert.assertEquals(18, bookdao.insert(entity));
+////        } finally {
+////            session.close();
+////        }
+////    }
+////
+////    @Test
+////    public void deleteTest() {
+////        SqlSession session=MyBatisUtil.getSession();
+////        try {
+////            GoodsDAO bookdao=session.getMapper(GoodsDAO.class);
+////            Assert.assertEquals(8, bookdao.delete(12));
+////        } finally {
+////            session.close();
+////        }
+////    }
+////
+////    @Test
+////    public void update() {
+////        SqlSession session=MyBatisUtil.getSession();
+////        try {
+////            GoodsDAO bookdao=session.getMapper(GoodsDAO.class);
+////            Goods entity=bookdao.getGoodsById(3);
+////            entity.setName("名字一定要超级的长");
+////            entity.setPrice(107);
+////            entity.setPicture("nopicture.jpg");
+////
+////            Assert.assertEquals(1, bookdao.update(entity));
+////        } finally {
+////            session.close();
+////        }
+////    }
 //    
-//    @Test
-//    public void getGoodsCountTest() {
-//        SqlSession session=MyBatisUtil.getSession();
-//        try {
-//            GoodsDAO bookdao=session.getMapper(GoodsDAO.class);
-//            Assert.assertEquals(9, bookdao.getGoodsCount());
-//        } finally {
-//            session.close();
-//        }
-//    }
-//    
-//    @Test
-//    public void insertTest() {
-//        SqlSession session=MyBatisUtil.getSession();
-//        try {
-//            Goods entity=new Goods();
-//            entity.setName("m俺妈说说名字必须的长");
-//            entity.setPrice(108);
-//            entity.setPicture("nopic.jpg");
-//            GoodsDAO bookdao=session.getMapper(GoodsDAO.class);
-//            Assert.assertEquals(18, bookdao.insert(entity));
-//        } finally {
-//            session.close();
-//        }
-//    }
-//
-//    @Test
-//    public void deleteTest() {
-//        SqlSession session=MyBatisUtil.getSession();
-//        try {
-//            GoodsDAO bookdao=session.getMapper(GoodsDAO.class);
-//            Assert.assertEquals(8, bookdao.delete(12));
-//        } finally {
-//            session.close();
-//        }
-//    }
-//
-//    @Test
-//    public void update() {
-//        SqlSession session=MyBatisUtil.getSession();
-//        try {
-//            GoodsDAO bookdao=session.getMapper(GoodsDAO.class);
-//            Goods entity=bookdao.getGoodsById(3);
-//            entity.setName("名字一定要超级的长");
-//            entity.setPrice(107);
-//            entity.setPicture("nopicture.jpg");
-//
-//            Assert.assertEquals(1, bookdao.update(entity));
-//        } finally {
-//            session.close();
-//        }
-//    }
-    
-}
+//}
