8bece14b5a8b28d2a1b88e563638a3c8623ef128
欧阳明择 <enforceway@163.com>
2021-01-30 06:08:18 +0800

    修复macos下自动配置不成功，无法启动报错问题。

--- src/main/java/com/webdiapp/WebdiappApplication.java
+++ src/main/java/com/webdiapp/WebdiappApplication.java
@@ -1,11 +1,12 @@
 package com.webdiapp;
 
 import org.mybatis.spring.annotation.MapperScan;
-import org.mybatis.spring.annotation.MapperScans;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 @MapperScan({"com.webdiapp.questionaire.mapper","com.webdiapp.question.mapper","com.webdiapp.questionItem.mapper","com.webdiapp.user.mapper","com.webdiapp.roleAccess.mapper"})
 public class WebdiappApplication {
 
